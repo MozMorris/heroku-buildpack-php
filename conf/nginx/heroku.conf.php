14f2efd26589bc6dd32c496cf1ae3489b4696084
Moz Morris <moz@earthview.co.uk>
2015-01-20 18:55:44 +0000

    Debug log.

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -51,8 +51,8 @@ http {
         port_in_redirect off;
         
         root "<?=getenv('DOCUMENT_ROOT')?:getenv('HEROKU_APP_DIR')?:getcwd()?>";
-        
-        error_log stderr;
+
+        error_log /tmp/heroku.nginx_error.<?=getenv('PORT')?:'8080'?>.log debug;
         access_log /tmp/heroku.nginx_access.<?=getenv('PORT')?:'8080'?>.log;
         
         include "<?=getenv('HEROKU_PHP_NGINX_CONFIG_INCLUDE')?>";
